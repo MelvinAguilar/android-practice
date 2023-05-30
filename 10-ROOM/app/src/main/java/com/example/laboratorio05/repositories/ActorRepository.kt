037c8b5256a92126a2655106d8db3176afe2a826
MelvinAguilar <00067621@uca.edu.sv>
2023-05-30 11:54:35 -0600

    Upload labo 10

---
+++ 10-ROOM/app/src/main/java/com/example/laboratorio05/repositories/ActorRepository.kt
@@ -0,0 +1,12 @@
+package com.example.laboratorio05.repositories
+
+import com.example.laboratorio05.data.dao.ActorDao
+import com.example.laboratorio05.data.model.ActorModel
+
+class ActorRepository(private val actorsDao: ActorDao) {
+
+    suspend fun getActors() = actorsDao.getAllActors()
+
+    suspend fun addActors(actor: ActorModel) = actorsDao.insertActor(actor)
+
+}
